60a0f1381b5b2c856115c85006a41eeff546289f
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-11-29 16:32:50 +1100

    Updated NavBar

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -3,28 +3,49 @@ import "./header.css";
 
 function Header() {
   return (
-    <header>
-      <div className="container">
-        <div id="branding">
-          <h1>
-            <span className="highlight">Complete</span> Pool Services
-          </h1>
-        </div>
-        <nav>
-          <ul>
-            <li className="current">
-              <h4>Home</h4>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div class="container">
+        <a href="index.html" class="navbar-brand">
+          Complete Pool Care
+        </a>
+        <button
+          class="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a href="index.html" class="nav-link">
+                Home
+              </a>
             </li>
-            <li>
-              <h4>About</h4>
+            <li class="nav-item">
+              <a href="about.html" class="nav-link">
+                About
+              </a>
             </li>
-            <li>
-              <h4>Services</h4>
+            <li class="nav-item">
+              <a href="services.html" class="nav-link">
+                Services
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="blog.html" class="nav-link">
+                Blog
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.html" class="nav-link">
+                Contact
+              </a>
             </li>
           </ul>
-        </nav>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 }
 
