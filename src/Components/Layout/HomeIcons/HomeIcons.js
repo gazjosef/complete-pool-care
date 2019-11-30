da67015ccbbd98843574ab49910d1ed099ce1716
gazjosef <39395712+gazjosef@users.noreply.github.com>
2019-11-30 22:31:37 +1100

    Updated HomeIcons

--- src/Components/Layout/HomeIcons/HomeIcons.js
+++ src/Components/Layout/HomeIcons/HomeIcons.js
@@ -6,24 +6,24 @@ const HomeIcons = () => {
       <div className="container">
         <div className="row">
           <div className="col-md-4 mb-4 text-center">
-            <i className="fas fa-cog fa-3x mb-2"></i>
-            <h3>Turning Gears</h3>
+            <i className="fas fa-swimmer fa-3x mb-2"></i>
+            <h3>Cleaning</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Temporibus, illo!
             </p>
           </div>
           <div className="col-md-4 mb-4 text-center">
-            <i className="fas fa-cloud fa-3x mb-2"></i>
-            <h3>Sending Data</h3>
+            <i className="fas fa-wrench fa-3x mb-2"></i>
+            <h3>Repair</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Temporibus, illo!
             </p>
           </div>
           <div className="col-md-4 mb-4 text-center">
-            <i className="fas fa-cart-plus fa-3x mb-2"></i>
-            <h3>Making Money</h3>
+            <i className="fas fa-shopping-cart fa-3x mb-2"></i>
+            <h3>Shop</h3>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Temporibus, illo!
