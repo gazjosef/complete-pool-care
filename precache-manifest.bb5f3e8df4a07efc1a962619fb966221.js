4ab1bc147feae09055c3f8723efff080accba096
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-01-28 21:42:45 +1100

    Updates

---
+++ precache-manifest.bb5f3e8df4a07efc1a962619fb966221.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b95886a0fee32a45f65500cc639c4e",
+    "url": "/complete-pool-care/index.html"
+  },
+  {
+    "revision": "35193ea7731fdca4cddb",
+    "url": "/complete-pool-care/static/css/main.8edfcd2b.chunk.css"
+  },
+  {
+    "revision": "0e222a83238b3e59e92f",
+    "url": "/complete-pool-care/static/js/2.70738b0d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/complete-pool-care/static/js/2.70738b0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35193ea7731fdca4cddb",
+    "url": "/complete-pool-care/static/js/main.8e027f02.chunk.js"
+  },
+  {
+    "revision": "8434eaa83baee0fd049d",
+    "url": "/complete-pool-care/static/js/runtime-main.66a3ad36.js"
+  },
+  {
+    "revision": "78d7b0dfa3e3d7c956eea6cfbec668ae",
+    "url": "/complete-pool-care/static/media/media.78d7b0df.jpg"
+  },
+  {
+    "revision": "71b0dad0a740e678440a456d62c9ff03",
+    "url": "/complete-pool-care/static/media/swim-422546_1920.71b0dad0.jpg"
+  },
+  {
+    "revision": "c7efe04f5df34d8daf9ec8c62e28d71d",
+    "url": "/complete-pool-care/static/media/swimming-pool-1031245_1920.c7efe04f.jpg"
+  },
+  {
+    "revision": "eda099932a8e62a0375f25afd5f60cc4",
+    "url": "/complete-pool-care/static/media/swimming-pool-389267_1280.eda09993.jpg"
+  }
+]);
