6008c9782df7fa0c179b9e74e7b7b893acaeb92c
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-01-28 22:16:56 +1100

    Updated Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+// Layout
+import Showcase from "../Layout/Showcase/Showcase";
+import HomeIcons from "../Layout/HomeIcons/HomeIcons";
+import Video from "../Layout/Video/Video";
+import Gallery from "../Layout/Gallery/Gallery";
+
 export const Home = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <Showcase />
+      <HomeIcons />
+      <Video />
+      <Gallery />
+    </div>
+  );
 };
