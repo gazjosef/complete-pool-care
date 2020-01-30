cee489ff25eccfb9f44b07c6d5b4001c5ab9efcf
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-01-30 21:55:51 +1100

    Updated Testimonials Section

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,12 +1,51 @@
 import React, { Fragment } from "react";
+import "./about.css";
 
 const About = () => {
   return (
     <Fragment>
       <section className="testimonials">
         <div className="container">
-          <h1>testimonials</h1>
+          <h1>Testimonials</h1>
           <p className="text-center">Subscribe</p>
+          <div className="row">
+            <div className="col-md-4 text-center">
+              <div className="profile">
+                <img src="" alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repudiandae accusamus sequi dolorum quo, numquam dignissimos?
+                </blockquote>
+                <h3>
+                  John Smith <span>Co-founder</span>
+                </h3>
+              </div>
+            </div>
+            <div className="col-md-4 text-center">
+              <div className="profile">
+                <img src="" alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repudiandae accusamus sequi dolorum quo, numquam dignissimos?
+                </blockquote>
+                <h3>
+                  John Smith <span>Co-founder</span>
+                </h3>
+              </div>
+            </div>
+            <div className="col-md-4 text-center">
+              <div className="profile">
+                <img src="" alt="" className="user" />
+                <blockquote>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Repudiandae accusamus sequi dolorum quo, numquam dignissimos?
+                </blockquote>
+                <h3>
+                  John Smith <span>Co-founder</span>
+                </h3>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </Fragment>
